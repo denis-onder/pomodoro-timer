37a48d1f1cc5dbb8340624573be23599084b2f1c
Denis Onder <denis_onder@protonmail.com>
2020-04-10 11:42:34 +0200

    Updated install script

--- install.sh
+++ install.sh
@@ -11,6 +11,10 @@ rm pomodoro-timer # Remove any old executables laying around
 go build .
 sudo mv ./pomodoro-timer /usr/local/bin
 
+# Copy sound file to where the executable is
+sudo rm /usr/local/bin/ding.mp3 # Remove any old sound files
+sudo cp ./ding.mp3 /usr/local/bin
+
 # Notify user that it's all done
 echo "Moved the binary to /usr/local/bin"
 echo "You can now use pomodoro-timer from anywhere in your terminal!"
